4469493e5ee7682215af4d201cd3f5d40293e3b7
karla332 <kmilanca21@gmail.com>
2026-07-09 12:33:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,32 @@ try:
         folium.raster_layers.ImageOverlay(image=colored_dem, bounds=[[src.bounds.bottom, src.bounds.left], [src.bounds.top, src.bounds.right]], name="Relieve (DEM)", opacity=0.4).add_to(m)
 except: pass
 
-# 5.
+# 5. Capas vectoriales
+folium.GeoJson(reserva, name="Reserva", style_function=lambda x: {'fillColor': 'transparent', 'color': '#004d00', 'weight': 3}).add_to(m)
+folium.GeoJson(rios, name="Ríos", style_function=lambda x: {'color': '#00BFFF', 'weight': 2, 'opacity': 0.8}).add_to(m)
+
+# 6. Especies con Imágenes
+for _, row in especies[especies['common_name'].isin(seleccion)].iterrows():
+    nombre = str(row.get('common_name', 'Especie'))
+    color = '#FF1493' if 'Alerce' in nombre else ('purple' if 'Ranita' in nombre else ('orange' if 'Chucao' in nombre else 'gray'))
+    html = f'<div style="width:150px;"><h4>{nombre}</h4><img src="{row.get("image_url", "")}" style="width:100%; border-radius:5px;"></div>'
+    folium.CircleMarker([row.geometry.y, row.geometry.x], radius=7, color=color, fill=True, popup=folium.Popup(html, max_width=200)).add_to(m)
+
+# 7. Elementos finales
+FloatImage("https://raw.githubusercontent.com/sjauregui/folium_examples/master/north_arrow.png", bottom=90, left=10).add_to(m)
+folium.ControlScale(position='bottomleft').add_to(m)
+
+legend_html = '''
+     <div style="position: fixed; bottom: 50px; left: 50px; z-index:9999; font-size:12px; background:white; padding:10px; border-radius:5px; border:1px solid #ccc; color: black;">
+      <b style="color: black;">Leyenda:</b><br>
+      <i class="fa fa-minus" style="color:#00BFFF"></i> <span style="color: black;">Ríos</span><br>
+      <i class="fa fa-square" style="color:green"></i> <span style="color: black;">Árido</span><br>
+      <i class="fa fa-square" style="color:red"></i> <span style="color: black;">Denso</span><br>
+      <i class="fa fa-circle" style="color:#FF1493"></i> <span style="color: black;">Alerce</span><br>
+      <i class="fa fa-circle" style="color:purple"></i> <span style="color: black;">Ranita</span><br>
+      <i class="fa fa-circle" style="color:orange"></i> <span style="color: black;">Chucao</span>
+     </div>'''
+m.get_root().html.add_child(folium.Element(legend_html))
+
+folium.LayerControl(collapsed=False).add_to(m)
+st_folium(m, width=900, height=600)
