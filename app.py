35a7234616e82315a8ba639b2b9a751d434de82c
karla332 <kmilanca21@gmail.com>
2026-07-09 12:21:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 st.set_page_config(layout="wide")
 st.title("🌿 Visualizador Ambiental: Reserva Nacional Alerce Costero")
 
-# 1. Carga de datos (La original que te funcionaba)
+# 1. Carga de datos directa
 def load_gpkg(path):
     if not os.path.exists(path):
         st.error(f"No se encuentra el archivo: {path}")
@@ -61,4 +61,25 @@ for _, row in especies[especies['common_name'].isin(seleccion)].iterrows():
     nombre = str(row.get('common_name', 'Especie'))
     color = 'red' if 'Alerce' in nombre else ('purple' if 'Ranita' in nombre else ('orange' if 'Chucao' in nombre else 'gray'))
     html = f'<div style="width:150px;"><h4>{nombre}</h4><img src="{row.get("image_url", "")}" style="width:100%; border-radius:5px;"></div>'
-    folium.
+    folium.CircleMarker([row.geometry.y, row.geometry.x], radius=7, color=color, fill=True, popup=folium.Popup(html, max_width=200)).add_to(m)
+
+# 7. Elementos finales con leyenda completa
+FloatImage("https://raw.githubusercontent.com/sjauregui/folium_examples/master/north_arrow.png", bottom=90, left=10).add_to(m)
+legend_html = '''
+     <div style="position: fixed; bottom: 50px; left: 50px; z-index:9999; font-size:12px; background:white; padding:10px; border-radius:5px; border:1px solid #ccc; color: black;">
+      <b style="color: black;">Leyenda:</b><br>
+      <i class="fa fa-minus" style="color:#00BFFF"></i> <span style="color: black;">Ríos</span><br>
+      <hr style="margin: 5px 0;">
+      <b style="color: black;">Vegetación:</b><br>
+      <i class="fa fa-square" style="color:green"></i> <span style="color: black;">Árido</span><br>
+      <i class="fa fa-square" style="color:red"></i> <span style="color: black;">Denso</span><br>
+      <hr style="margin: 5px 0;">
+      <b style="color: black;">Especies:</b><br>
+      <i class="fa fa-circle" style="color:red"></i> <span style="color: black;">Alerce</span><br>
+      <i class="fa fa-circle" style="color:purple"></i> <span style="color: black;">Ranita</span><br>
+      <i class="fa fa-circle" style="color:orange"></i> <span style="color: black;">Chucao</span>
+     </div>'''
+m.get_root().html.add_child(folium.Element(legend_html))
+
+folium.LayerControl(collapsed=False).add_to(m)
+st_folium(m, width=900, height=600)
